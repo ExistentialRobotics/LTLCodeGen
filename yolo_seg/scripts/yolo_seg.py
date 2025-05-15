e804c2a0955001babe83540335bb2160f0acfeb8
Behrad Rabiei <74337784+brabiei21@users.noreply.github.com>
2025-05-14 19:53:57 -0700

    Update yolo_seg.py

--- yolo_seg/scripts/yolo_seg.py
+++ yolo_seg/scripts/yolo_seg.py
@@ -2,25 +2,32 @@
 
 import rospy
 import numpy as np
-import yaml
 import cv2
 import cv_bridge
-import sensor_msgs.msg as sensor_msgs
-import vision_msgs.msg as vision_msgs
+from sensor_msgs.msg import Image
+import message_filters
 
 from ultralytics import YOLO
+import logging
+
+# Suppress Ultralytics log output
+# logging.getLogger('ultralytics').setLevel(logging.CRITICAL)
 
 class YoloSeg:
     def __init__(self, weights):
         self.model = YOLO(weights)
+        # self.model.export(format='engine')
+        # self.tensor_rt_model = YOLO('yolo11x-seg.engine')
     
-    def detect(self, img, img_size = (360,480,3)):
-        results = self.model(img, conf=0.25)
+    def detect(self, img, img_size = (480,640,3)):
+        results = self.model(img, conf=0.8)
+        # results = self.tensor_rt_model(img, conf=0.8)
+        # print(f"Current Device: {self.model.device}")
         if results[0].masks:  # Check if masks exist
             try:
                 masks = results[0].masks.data.cpu().numpy()
                 combined_mask = np.ones((*masks[0].shape, 3), dtype=np.uint8)
-                label_mask = 80 * np.ones((*masks[0].shape, 3), dtype=np.uint8)
+                label_mask = 100 * np.ones((*masks[0].shape, 3), dtype=np.uint8) # The number is so that we can exceed all possible class ids
                 mask_ids = results[0].boxes.cls.cpu().numpy().astype(dtype=np.uint).tolist()
                 
                 # Combine masks and assign unique colors to each label
@@ -34,39 +41,82 @@ class YoloSeg:
             
             except Exception as e:
                 rospy.logerr(f"Error processing masks: {e}")
-                return None
-        return np.zeros(img_size, dtype=np.uint8) # black image for capturing depth information
+                return  np.zeros(img_size, dtype=np.uint8),  np.zeros(img_size, dtype=np.uint8)
+
+        return np.zeros(img_size, dtype=np.uint8),  np.zeros(img_size, dtype=np.uint8) # black image for capturing depth information
 
 class Segmenter:
     def __init__(self):
         self.bridge = cv_bridge.CvBridge()
+
         # Read parameters
         self._weights = rospy.get_param('~weights', 'yolov8x-seg.pt')
-        self._input_topic = rospy.get_param('~input_topic', '/oakd/rgb/preview/image_raw')
-        self._output_topic = rospy.get_param('~output_topic', 'segmented_image')
-        self._class_topic = rospy.get_param('~class_topic', 'class_image')
-        # Load Yolo model
+        self._input_rgb_topic = rospy.get_param('~input_rgb_topic', 'camera/color/img_raw')
+        self._input_depth_topic = rospy.get_param('~input_depth_topic', 'camera/depth/img_raw')
+        self._output_seg_topic = rospy.get_param('~output_segmentation_topic', 'segmented_image')
+        self._output_rgb_topic = rospy.get_param('~output_rgb_topic', 'rgb_image')
+        self._output_depth_topic = rospy.get_param('~output_depth_topic', 'depth_image')
+        # self._class_topic = rospy.get_param('~class_topic', 'class_image') # this is for simulation
+
+        # Load YOLO model
         self.segmenter = YoloSeg(self._weights)
 
         # ROS communication
-        self.sub_img_in = rospy.Subscriber(self._input_topic, sensor_msgs.Image, self.img_cb, queue_size=1)
-        self.pub_img_out = rospy.Publisher(self._output_topic, sensor_msgs.Image, queue_size=1)
-        self.pub_img_class = rospy.Publisher(self._class_topic, sensor_msgs.Image, queue_size=1)
+        rgb_sub = message_filters.Subscriber(self._input_rgb_topic, Image)
+        depth_sub = message_filters.Subscriber(self._input_depth_topic, Image)
 
-    def img_cb(self, msg):
+        # Synchronize RGB and depth topics
+        self.ts = message_filters.ApproximateTimeSynchronizer([rgb_sub, depth_sub], queue_size=1, slop=0.1)
+        self.ts.registerCallback(self.callback)
+
+        # Publishers
+        self.pub_seg_out = rospy.Publisher(self._output_seg_topic, Image, queue_size=1)
+        self.pub_rgb_out = rospy.Publisher(self._output_rgb_topic, Image, queue_size=1)
+        self.pub_depth_out = rospy.Publisher(self._output_depth_topic, Image, queue_size=1)
+        # self.pub_img_class = rospy.Publisher(self._class_topic, Image, queue_size=1)
+
+    def callback(self, rgb_msg, depth_msg):
         try:
-            frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            detections, labels = self.segmenter.detect(frame)
-            out = self.bridge.cv2_to_imgmsg(detections, encoding='bgr8')
-            out.header = msg.header
-            class_msg = self.bridge.cv2_to_imgmsg(labels, encoding='bgr8')
-            class_msg.header = msg.header
-            self.pub_img_out.publish(out)
-            self.pub_img_class.publish(class_msg)
-        except Exception as e:
-            rospy.logerr(f"Error in image callback: {e}")
+            # Convert RGB image to OpenCV format
+            color_image = self.bridge.imgmsg_to_cv2(rgb_msg, desired_encoding='bgr8')
+            color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
 
+            # Flip color image vertically because camera on jackal is upside down
+            color_image = cv2.flip(color_image, 0)
+
+            # Perform segmentation
+            detections, _ = self.segmenter.detect(color_image, color_image.shape)
+            print(f"Image Size: {color_image.shape}")
+
+        
+            # Flip detections back to original orientation
+            detections = cv2.flip(detections, 0)
+            
+            # Convert segmentation result to ROS Image message
+            color_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2BGR)
+            seg_msg = self.bridge.cv2_to_imgmsg(detections, encoding='bgr8')
+            seg_msg.header = rgb_msg.header
+
+            # Convert class labels to ROS Image message
+            # class_msg = self.bridge.cv2_to_imgmsg(labels, encoding='bgr8')
+            # class_msg.header = rgb_msg.header
+
+            # Publish segmentation image
+            self.pub_seg_out.publish(seg_msg)
+
+            # Publish original RGB image
+            self.pub_rgb_out.publish(rgb_msg)
+
+            # Publish synchronized depth image
+            self.pub_depth_out.publish(depth_msg)
+
+            # Publish class labels image
+            # self.pub_img_class.publish(class_msg)
+
+        except Exception as e:
+            rospy.logerr(f"Error in synchronized callback: {e}")
+            
+            
 if __name__ == "__main__":
     
     rospy.init_node("yolo_seg_node")
